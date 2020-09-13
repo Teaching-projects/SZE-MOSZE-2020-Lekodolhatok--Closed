79754f580143bbbe88919c9b0153c1e7f124c889
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 17:41:13 +0200

    Get arguments and create characters, create fight logic

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include "character.h"
+#include <list>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	Character ch1("Maple", 150, 10);
-	Character ch2("Sally", 45, 30);
 
-	std::cout << ch1;
-	std::cout << ch2;
+	list<Character> characters;
+
+	for (int i = 0; i < argc/3; i++)
+	{
+		characters.push_back(Character((argv[(i*2) + 1], argv[(i*2) + 2], argv[(i*2) + 3]));
+	}
+
+//	Character ch1("Maple", 150, 10);
+	//Character ch2("Sally", 45, 30);
+
+	std::cout << characters[0];
+	std::cout << characters[0];
 
 	ch2.AttackEnemy(ch1);
 
