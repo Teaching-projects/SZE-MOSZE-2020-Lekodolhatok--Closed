1a4bbcdee429c1a63c04274ae0e86dc4886c3da8
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 16:49:38 +0200

    Create characters other way

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-	Character ch1 = new Character("Maple", 150, 10);
-	Character ch2 = new Character("Sally", 45, 30);
+	Character ch1("Maple", 150, 10);
+	Character ch2("Sally", 45, 30);
 
 	std::cout << ch1;
 	std::cout << ch2;
