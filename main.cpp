5b393e50b5ca46b6616cebd65420a554827b3adf
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 16:35:19 +0200

    Include character.h and create hard coded char., print them.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "character.h"
 
 using namespace std;
 
 int main() {
-	cout << "Hello Lekodolhatók!";
+	Character ch1 = new Character("Maple", 150, 10);
+	Character ch2 = new Character("Sally", 45, 30);
 
+	std::cout << ch1;
+	std::cout << ch2;
 }
