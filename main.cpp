0dd3a0a4e7427fb583679d5823b32477d2e7f095
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 17:04:39 +0200

    Start fight logic + add starting arguments

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 	Character ch1("Maple", 150, 10);
 	Character ch2("Sally", 45, 30);
 
 	std::cout << ch1;
 	std::cout << ch2;
+
+	ch2.AttackEnemy(ch1);
+
+	do
+	{
+
+	} while (!ch1.isDead() || !ch2.isDead());
 }
