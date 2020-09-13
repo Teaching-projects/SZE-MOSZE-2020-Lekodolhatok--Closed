ea76d1dc403302b7cf8e408ff4cc7c644efc2476
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 18:06:07 +0200

    use vector size() function instead of argc

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	vector<Character> characters;
-
 	for (int i = 0; i < argc / 3; i++)
 	{
 		std::string name(argv[(i * 2) + 1 + i]);
@@ -25,17 +24,18 @@ int main(int argc, char* argv[]) {
 	int roundCounter = 0;
 	do
 	{
-		
-		if (roundCounter + 1 >= argc) {
+
+		if (roundCounter + 1 >= characters.size()) {
 			characters[roundCounter].AttackEnemy(characters[0]);
 			roundCounter = 0;
 		}
 		else {
-			characters[roundCounter].AttackEnemy(characters[roundCounter+1]);
+			characters[roundCounter].AttackEnemy(characters[roundCounter + 1]);
 			roundCounter += 1;
 		}
 		for (auto character : characters)
 		{
+			cout << character;
 			if (character.isDead()) {
 				isDead = true;
 			}
