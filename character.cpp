cb5b618897b3442a1e55d6c71e4942af1ed148db
Stefka Márton <stefkamarton14@gmail.com>
2020-09-13 16:07:35 +0200

    Implement Character class functions

--- character.cpp
+++ character.cpp
@@ -1,3 +1,13 @@
 #include "character.h"
 
+std::string Character::getName() {
+	return Character::Name;
+}
 
+int Character::getHP() {
+	return Character::HP;
+}
+
+int Character::getDMG() {
+	return Character::getDMG();
+}
