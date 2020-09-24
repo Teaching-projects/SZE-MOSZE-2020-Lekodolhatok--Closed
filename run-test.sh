a998997b4aa67b114aca7e789c8ffc2a596606c6
Stefka Márton <stefkamarton14@gmail.com>
2020-09-24 23:42:11 +0200

    Add bash script + check.txt

--- run-test.sh
+++ run-test.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+touch test/results.log
+
+declare -a arr=("kakarott.json" "vakarott.json" "makarott.json")
+
+for i in "${arr[@]}"
+do
+    for j in "${arr[@]}"
+    do
+        if [[ "$i" != "$j" ]]
+        then
+            echo "$i" "$j"
+            ./a.out "$i" "$j" >> test/results.log
+        fi
+    done
+done
+
+#./a.out kakarott.json
