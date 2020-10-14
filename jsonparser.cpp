cab82c9675d7294d90c2a91829c51fbc3298a1b0
Stefka Márton <stefkamarton14@gmail.com>
2020-10-14 23:03:37 +0200

    Fix bugs in jsonparser

--- jsonparser.cpp
+++ jsonparser.cpp
@@ -1,7 +1,7 @@
 #include "jsonparser.h"
 
-std::map<std::string, std::any>& Json::ParseUnit(std::ifstream& unit) {
-	std::map<std::string, std::any> d;
+std::map<std::string, std::string> Json::ParseUnit(std::ifstream& unit) {
+	std::map<std::string, std::string> d;
 	std::string name;
 	int hp = 0;
 	int dmg = 0;
@@ -15,60 +15,19 @@ std::map<std::string, std::any>& Json::ParseUnit(std::ifstream& unit) {
 	if (!unit.fail() && unit.is_open()) {
 		while (std::getline(unit, line))
 		{
-
-			std::map<std::string, std::any> seged = Json::ParseUnit(line);
+			std::map<std::string, std::string>& seged = Json::ParseUnit(line);
 			d.insert(seged.begin(), seged.end());
-			//d.insert(seged.begin(), seged.end());
-			/*
-			int first = line.find("\"", 0);
-			int second = line.find("\"", first + 1);
-			int third = line.find("\"", second + 1);
-			int fourth;
-			bool isNumber = false;
-			if (third <= 0) {
-				third = line.find(":", second + 1);
-				fourth = line.find(",", third + 1);
-				isNumber = true;
-			}
-			else {
-				fourth = line.find("\"", third + 1);
-			}
-			if (first >= 0 && second >= 0 && third >= 0) {
-				std::cout << "1:" << line.substr(first + 1, second - first - 1) << std::endl;
-				std::cout << "2:" << line.substr(third + 1, fourth - third - 1) << std::endl;
-				std::string key = line.substr(first + 1, second - first - 1);
-				std::string value;
-				if (!isNumber) {
-					value = line.substr(third + 1, fourth - third - 1);
-				}
-				else {
-					value = std::stod(line.substr(third + 1));
-				}
-				d.insert(std::pair<std::string, std::any>(key, value));
-			}
-			std::cout << "LINE.1" << line << "\t";
-			line.erase(0, fourth + 1);
-			std::cout << "LINE.2" << line << "\n";*/
-		}
-	}
-	for (auto elem : d)
-	{
-		if (elem.second.type() == typeid(double)) {
-			std::cout << elem.first << std::any_cast<double>(elem.second) << "\n";
-		}
-		else if (elem.second.type() == typeid(std::string)) {
-			std::cout << elem.first << std::any_cast<std::string>(elem.second) << "\n";
 		}
 	}
 	return d;
 }
-std::map<std::string, std::any> Json::ParseUnit(std::string& line) {
+std::map<std::string, std::string> Json::ParseUnit(std::string& line) {
 	std::ifstream unit("units/" + line);
 	if (!unit.fail() && unit.is_open()) {
 		Json::ParseUnit(unit);
 	}
 
-	std::map<std::string, std::any> d;
+	std::map<std::string, std::string> d;
 	int first;
 	int second;
 	int third;
@@ -80,7 +39,6 @@ std::map<std::string, std::any> Json::ParseUnit(std::string& line) {
 		third = line.find("\"", second + 1);
 		fourth;
 		bool noend = false;
-		bool isNumber = false;
 		int nextcomma = line.find(",", second + 1);
 		if (nextcomma < 0) {
 			nextcomma = line.find("}", second + 1);
@@ -91,7 +49,6 @@ std::map<std::string, std::any> Json::ParseUnit(std::string& line) {
 		if (third <= 0 || nextcomma < third) {
 			third = line.find(":", second + 1);
 			fourth = nextcomma;
-			isNumber = true;
 		}
 		else {
 			fourth = line.find("\"", third + 1);
@@ -105,12 +62,9 @@ std::map<std::string, std::any> Json::ParseUnit(std::string& line) {
 			else {
 				value = line.substr(third + 1);
 			}
-			if (isNumber) {
-				d.insert(std::pair<std::string, std::any>(key, std::stod(value)));
-			}
-			else {
-				d.insert(std::pair<std::string, std::any>(key, value));
-			}
+			value.erase(remove_if(value.begin(), value.end(), isspace));
+
+			d.insert(std::pair<std::string, std::string>(key, value));
 		}
 		line.erase(0, fourth + 1);
 	} while (first > 0 && second > 0 && third > 0 && fourth > 0);
--- jsonparser.h
+++ jsonparser.h
@@ -4,14 +4,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <any>
 #include <variant>
 class Json {
-private:
-	std::pair<std::string, std::any>& Pairs(const std::string& line);
 public:
-	static std::map<std::string, std::any> ParseUnit(std::string&);
-	static std::map<std::string, std::any>& ParseUnit(std::ifstream&);
+	static std::map<std::string, std::string> ParseUnit(std::string&);
+	static std::map<std::string, std::string> ParseUnit(std::ifstream&);
 };
 
 #endif // !JSON_H
