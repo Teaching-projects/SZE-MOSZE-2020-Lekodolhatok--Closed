7d3dfde453a957aa01f77e8509164d7a9cc56c02
Stefka Márton <stefkamarton14@gmail.com>
2020-10-14 23:05:11 +0200

    Unit test

--- test/jsonTest.cpp
+++ test/jsonTest.cpp
@@ -1,7 +1,32 @@
 #include <gtest/gtest.h>
-#include "jsonparser.h"
+#include "../jsonparser.h"
+#include <any>
+#include <map>
+#include <string>
+#include <fstream>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(JsonParser, FromFileName) {
+    std::map<std::string, std::string> example;
+    example.insert(std::pair<std::string, std::string>("name", "Kakarott"));
+    example.insert(std::pair<std::string, std::string>("hp", "50000"));
+    example.insert(std::pair<std::string, std::string>("dmg", "9000"));
+
+    std::string s = "{\"name\" : \"Kakarott\",\"hp\" : \"50000\",\"dmg\" : \"9000\"}";
+    std::map<std::string, std::string> result = Json::ParseUnit(s);
+    ASSERT_EQ(example, result);
+};
+TEST(JsonParser, FromStream) {
+    std::map<std::string, std::string> example;
+    example.insert(std::pair<std::string, std::string>("name", "Kakarott"));
+    example.insert(std::pair<std::string, std::string>("hp", "50000"));
+    example.insert(std::pair<std::string, std::string>("dmg", "9000"));
+
+    std::ifstream unit("../units/kakarott.json");
+    std::map<std::string, std::string> result = Json::ParseUnit(unit);
+    ASSERT_EQ(example, result);
+};
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
