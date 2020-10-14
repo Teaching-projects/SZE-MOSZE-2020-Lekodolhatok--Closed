e811a955fbb4805df21bc266c8972d91299a28c8
Stefka Márton <stefkamarton14@gmail.com>
2020-10-14 23:13:22 +0200

    Unit test

--- test/jsonTest.cpp
+++ test/jsonTest.cpp
@@ -25,6 +25,16 @@ TEST(JsonParser, FromStream) {
     std::map<std::string, std::string> result = Json::ParseUnit(unit);
     ASSERT_EQ(example, result);
 };
+TEST(JsonParser, FromString) {
+    std::map<std::string, std::string> example;
+    example.insert(std::pair<std::string, std::string>("name", "Kakarott"));
+    example.insert(std::pair<std::string, std::string>("hp", "50000"));
+    example.insert(std::pair<std::string, std::string>("dmg", "9000"));
+
+    std::string s = "{\"name\" : \"Kakarott\",\"hp\" : \"50000\",\"dmg\" : \"9000\"}";
+    std::map<std::string, std::string> result = Json::ParseUnit(s);
+    ASSERT_EQ(example, result);
+};
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
