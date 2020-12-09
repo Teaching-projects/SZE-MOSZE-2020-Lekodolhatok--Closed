b53914a5392f046d2117215cda142faa55ed2f74
Stefka Márton <stefkamarton14@gmail.com>
2020-12-09 14:00:42 +0100

    Fix 10

--- Hero.cpp
+++ Hero.cpp
@@ -34,8 +34,8 @@ void Hero::Attack(Monster& enemy) {
 }
 
 Hero Hero::parse(std::string& s) {
-	char c = new char[s.length()];
-	for (int i = 0; i < s.length; i++)
+	char* c = new char[s.length()];
+	for (int i = 0; i < s.length(); i++)
 	{
 		c[i] = s[i];
 	}
