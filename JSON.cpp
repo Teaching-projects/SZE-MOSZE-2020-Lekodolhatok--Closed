d1ca30ce9174b54f28541c1752476a91c9ebc903
Stefka Márton <stefkamarton14@gmail.com>
2020-12-09 17:06:04 +0100

    JSON

--- JSON.cpp
+++ JSON.cpp
@@ -44,14 +44,14 @@ std::map<std::string, std::string> JSON::parseFromString(std::string& line) {
 		if (third <= 0 || nextcomma < third) {
 			third = line.find(":", second + 1);
 			fourth = nextcomma;
-			num = true;
+			//num = true;
 		}
 		else {
 			fourth = line.find("\"", third + 1);
 		}
 		if (first >= 0 && second >= 0 && third >= 0) {
 			std::string key = line.substr(first + 1, second - first - 1);
-			std::variant<std::string,int> value;
+			std::string value;
 			if (!noend) {
 				if (num) {
 					value = std::stoi(line.substr(third + 1, fourth - third - 1));
@@ -70,7 +70,7 @@ std::map<std::string, std::string> JSON::parseFromString(std::string& line) {
 				}
 			}
 			value.erase(remove(value.begin(), value.end(), ' '), value.end());
-			d.insert(std::pair<std::string,std::variant<std::string,int>>(key, value));
+			d.insert(std::pair<std::string,std::string>(key, value));
 		}
 		line.erase(0, fourth + 1);
 	} while (first > 0 && second > 0 && third > 0 && fourth > 0);
